0fc9d0c0741c5f9982e6f57e3c143356e713c9ea
Marcin Szamotulski <mszamot@gmail.com>
2012-09-18 20:00:36 +0100

    Bug fix in atplib.atpvim.readlines()

--- ftplugin/ATP_files/atplib/atpvim.py
+++ ftplugin/ATP_files/atplib/atpvim.py
@@ -4,13 +4,23 @@ import vim
 
 __all__ = [ 'read', 'readlines', 'bufnumber' ]
 
+def bufnumber(fpath):
+    """ Return bufer number of fpath,
+
+    fpath should be a full path of a file.
+    """
+    for buf in vim.buffers:
+	if buf.name == fpath:
+            return buf.number
+    else:
+        return 0
 
 def read(file_path):
     """ Read lines from fname, check if the fname is loaded get tge buffer."""
 
-    for buf in vim.buffers:
-	if buf.name == file_path:
-            return '\n'.join(buf)
+    bufnr = bufname(file_path)
+    if bufnr:
+        return "\n".join(vim.buffers[bufnr-1])
     else:
         with open(file_path, 'r') as fo:
             # we are not decoding: since we have to assume that files are in &encoding
@@ -20,26 +30,11 @@ def read(file_path):
 def readlines(file_path):
     """ Read lines from fname, if the fname is loaded get the buffer."""
 
-    for buf in vim.buffers:
-	if buf.name == file_path:
-            return list(buf)
+    bufnr = bufnumber(file_path)
+    if bufnr:
+        return vim.buffers[bufnr-1]
     else:
         with open(file_path, 'r') as fo:
             # we are not decoding: since we have to assume that files are in &encoding
             # vim stores buffers, variables, ... in &encoding.
             return fo.readlines()
-
-def bufnumber(file, project_dir):
-    cdir = os.path.abspath(os.curdir)
-    os.chdir(project_dir)
-    for buf in vim.buffers:
-        # This requires that we are in the directory of the main tex file:
-	if buf.name == os.path.abspath(file):
-            os.chdir(cdir)
-            return buf.number
-    for buf in vim.buffers:
-	if os.path.basename(buf.name) == file:
-            os.chdir(cdir)
-            return buf.number
-    os.chdir(cdir)
-    return 0
